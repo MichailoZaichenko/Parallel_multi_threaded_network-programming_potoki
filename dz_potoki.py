e08ac9c2fdb58f538f29483bc30f7219579cab25
MichailoZaichenko <michailo.zaichenko@gmail.com>
2023-01-04 19:03:28 +0200

    add tasks

--- dz_potoki.py
+++ dz_potoki.py
@@ -3,3 +3,30 @@ import random
 l1 = threading.Lock()
 sm = threading.Semaphore(3)
 list1 = []
+
+def task1():
+    global l1
+    global list1
+    l1.acquire()
+    for i in range(10):
+        n = random.randint(1,10000)
+        list1.append(n)
+    print(f"Список: {list1} \n")
+    l1.release()
+
+
+def task2():
+    global l1
+    global list1
+    while l1.locked():
+        pass
+    summ = sum(list1)
+    print(f"Сумма: {summ}\n")
+
+def task3():
+    global l1
+    global list1
+    while l1.locked():
+        pass
+    aver = sum(list1)/len(list1)
+    print(f"Среднее: {aver}\n")
